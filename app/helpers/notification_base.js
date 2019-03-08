09194ea8b230c75d08c741a9f9131ebbb8ee30f8
akshayKoranga <44962164+akshayKoranga@users.noreply.github.com>
2019-03-08 15:25:41 +0530

    Update notification_base.js

--- app/helpers/notification_base.js
+++ app/helpers/notification_base.js
@@ -1,6 +1,6 @@
 function sendPush(device_token, payload_obj) {
     let FCM = require('fcm-node');
-    const serverKey = 'AAAAb-_MrjU:APA91bHO4tXMtGltgOuQXrRHWKZqM7g4XF-3EF2z4KBZa4q-dFwW6ZtU4xhpHaa2dgk2ONMOrhk9SYuD-5CvehE5gz2TPS_Y1eAO7MAxLRgFo3G_zmzv4nm_KBM7BzN4t4kXbnvXgybr';
+    const serverKey = '';
     const fcm = new FCM(serverKey);
     var result = JSON.stringify(payload_obj);
     var result = payload_obj;
